a5cc893a91f684e111bab0cee7e7b29b637b771d
Dan Neumann <danrodneu@gmail.com>
2026-06-24 11:53:23 -0500

    feat(app): scaffold Xcode iOS app project
    The boilerplate was created via XCode -> New and no further changes were
    made

---
+++ app/DanCam/DanCam/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DanCam
+//
+//  Created by dan on 6/24/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ app/DanCam/DanCamTests/DanCamTests.swift
@@ -0,0 +1,19 @@
+//
+//  DanCamTests.swift
+//  DanCamTests
+//
+//  Created by dan on 6/24/26.
+//
+
+import Testing
+@testable import DanCam
+
+struct DanCamTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
